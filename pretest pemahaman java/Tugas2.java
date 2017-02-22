a55c15fbb09fb7cd0bf2a9a30657a658b44d0a1a
Khanif Zyen <khanif.zyen@gmail.com>
2017-02-22 09:26:51 +0700

    Created by Coding Ground Client

---
+++ pretest pemahaman java/Tugas2.java
@@ -0,0 +1,9 @@
+public class Tugas2{
+
+     public static void main(String []args){
+        String[] nama = {"Agus","Budi","Khanif"};
+        System.out.println(nama[0]);
+        System.out.println(nama[1]);
+        System.out.println(nama[2]);
+     }
+}
